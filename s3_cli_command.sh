2daa93aa19baeaf7468ec4c3597a4309ef0a84de
Sai Karthik <144606889+saikarthiknaladala@users.noreply.github.com>
2023-10-23 00:55:32 -0500

    Update s3_cli_command.sh

--- s3_cli_command.sh
+++ s3_cli_command.sh
@@ -1,7 +1,7 @@
 #Replace It With Your Bucket Name
 
 # To copy all JSON Reference data to same location:
-aws s3 cp . s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+aws s3 cp . s3://de-utube-analysis-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
 # To copy all data files to its own location, following Hive-style patterns:
 aws s3 cp CAvideos.csv s3://de-utube-analysis-raw-useast1-dev/youtube/raw_statistics/region=ca/
